239711522e7ccbefa7d01aa2b5ed39d1b1e9ad69
Shobhit Mishra <shobhitmishra2.3.5@gmail.com>
2026-09-04 20:21:53 +0530

    Add ThowAndThrows2.java for exception handling demonstration

---
+++ Exception Handling/ThowAndThrows2.java
@@ -0,0 +1,20 @@
+public class ThowAndThrows2 {
+ public static void main(String[] args) {
+        checkEligibility(-5);
+
+        System.out.println("Back in main");
+    }
+
+ public static void checkEligibility(int age) {
+
+        try {
+            if (age <= 0) {
+                throw new IllegalArgumentException("Age cannot be negative");
+            }
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
