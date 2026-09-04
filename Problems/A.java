4ff6090491db6aa71d2d4e6b22838543887ad76c
Shobhit Mishra <shobhitmishra2.3.5@gmail.com>
2026-09-04 11:40:41 +0530

    corrected code

--- Problems/A.java
+++ Problems/A.java
@@ -1,12 +1,14 @@
 public class A {
     public static void main(String[] args) {
         DoSomething d1 = DoSomething.set(3, 4);
-        System.out.println(d1x + " " + d1y);
+        System.out.println(d1.x + " " + d1.y);
     }
 }
+
 class DoSomething {
-    static int x, y;
-    public static DoSomething(int x1, int y1) {
+    int x, y;
+
+    public static DoSomething set(int x1, int y1) {
         DoSomething k1 = new DoSomething();
         k1.x = x1;
         k1.y = y1;
